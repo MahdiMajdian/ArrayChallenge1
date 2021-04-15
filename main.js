e556dfab654e55146a79e98babdb373c6f86e115
Mahdi Majdian <mahdi2011.shz@gmail.com>
2021-04-16 02:15:53 +0430

    deleteByIndex done, combine and sort method done

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-console.log("Hello World")
-
 // 1- A function to delete an item by index
 
-function deleteByIndex(arr , index) { 
-arr.splice(index , 1)
-return arr
+function deleteByIndex(arr, index) {
+  arr.splice(index, 1);
+  return arr;
 }
 
-arr = ['a' , 'b' , 'c' , 'd']
-console.log(deleteByIndex(arr , 1))
+arr = ["a", "b", "c", "d"];
+console.log(deleteByIndex(arr, 1));
+
+// 2- A function to combine two or more arrays] and sort the final array
+function combine(...arg) {
+  let result = [];
+  for (let item of arg) {
+    result = result.concat(item);
+  }
+  result.sort((a, b) => a - b);
+  return result;
+}
+
+console.log(combine([3, 1, 2], [5, 6, 4], [7, 8, 9]));
