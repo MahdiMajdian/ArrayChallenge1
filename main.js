4ab24d58284779138d9075e6e1ebe733bd770c9f
Mahdi Majdian <mahdi2011.shz@gmail.com>
2021-04-16 10:24:11 +0430

    array methods done

--- main.js
+++ main.js
@@ -19,3 +19,33 @@ function combine(...arg) {
 }
 
 console.log(combine([3, 1, 2], [5, 6, 4], [7, 8, 9]));
+
+// 3- Remove duplicate items of an Array
+function duplicateRemover(array) {
+  let result = [];
+  for (let item of array) {
+    if (!result.includes(item)) {
+      result.push(item);
+    }
+  }
+  return result;
+}
+
+console.log(duplicateRemover([1, 1, 4, 5, 3, 5, 3, 9]));
+
+// 4- A function to extract numbers from a string
+
+function extract(string) {
+  let str = string.split("");
+  let numbers = str.filter(function isNumeric(item) {
+    return !isNaN(parseFloat(item)) && isFinite(item);
+  });
+  return numbers;
+}
+console.log(extract("i have 2 apples and 3 pineapples"));
+
+//5- Flat an Array of arrays
+
+let array = [[2, 3], 5, [[[2, 9], 4], 4], 1, 0];
+array.flat(Infinity);
+alert(array);
